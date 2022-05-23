3b12f7339114593453162c3422aa40a8e28cf912
ArbazParwani <arbazmiqbal@gmail.com>
2022-05-23 20:20:43 +0500

    Create assignment.js
    Assignment 1

---
+++ assignment.js
@@ -0,0 +1,325 @@
+//chp # 1//
+// Question 1//
+ ans1 :alert ("Hello") ;
+
+//Question 2//
+ans2 : alert ("please enter a valid password");
+
+//Question 3//
+ans3 :alert ("Welcome to js land. \n Happy coding");
+
+//Question 4//
+ans4: a:alert ("Welcome to js land") ;
+b: alert ("Happy coding");
+
+//Question 5//
+ans5:console.log ("Hello i can run Js through my web browser console");
+
+//Question 6//
+ans6: done 
+
+//Question 7//
+ans7: done
+
+
+//chp # 2//
+// Question 1//
+ans1: var userName
+
+// Question 2//
+ans2: var userName = "Arbaz" ;
+
+// Question 3//
+ans3: a: var message
+b: var message = "Hello World";
+c: alert (message);
+
+// Question 4//
+ans4: var nAme = "Arbaz";
+alert (nAme)
+var aGe = "20 years old";
+alert (aGe)
+var eduCAtion = "studing web and app development";
+alert (eduCAtion) ;
+
+// Question 5//
+ans5: var fOOd = "PIZZA\nPIZZ\nPIZ\nPI\nP" ;
+alert (fOOd) ;
+
+// Question 6//
+ans6:var eMail = "arbazmiqbal@gmail.com" ;
+alert ("my email address is " + eMail) ;
+
+// Question 7//
+ans7:var bOOk = "A smarter way to learn JavaScript" ;
+alert ("I learn JS From the book. " + bOOk) ;
+
+// Question 8//
+ans8:document.write ("<h1>Yah! I Can Write HTML Content Through JavaScript</h1>")
+
+// Question 9//
+ans9:var sP = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”" ;
+alert (sP) ;
+
+
+//chp # 3//
+// Question 1//
+ans:var aGe = 20 ;
+alert (aGe + " year old");
+
+// Question 2//
+ans: var visiTor =14
+alert ("You have visited this site " + visiTor + " times");
+
+// Question 3//
+ans:var birTHYear = 2002;
+document.write ("my birth year is " + birTHYear + " <br>Datatype of my declared variable is number");
+
+// Question 4//
+ans:var visiTorName = "John Doe" ;
+var proDucTTitle = " T-Shirt";
+var quanTiTy = 5; 
+document.write (visiTorName + " ordered " + quanTiTy + proDucTTitle + "(s) on XYZ clothing store") ;
+
+// chp # 4//
+// Question 1//
+ans: var x = 6 ;
+var y = 7 ;
+var z = 8 ;
+document.write(x + y+ z) ;
+
+// Question 2//
+ans: //legal variable//
+var name1 = "Arbaz" 
+var nAme  = "Danish"
+var na_me = "Asghar"
+var $name = "Dollar"
+var _name = "Shahzaib"
+//illegal variable//
+var n = "Arbaz"
+var 1name = "Shahzaib"
+var 123 = "ffgg"
+var na me  = "danish"
+var alert = "arbaz"
+
+// Question 3//
+document.write("<h1>Rules for naming JS variable</h1> <br>") ;
+document.write("Variable names can only contain. Number ,$ and _ . For example : $my_1stVaribale <br><br>") ;
+document.write("Variable must begin with letter, $ or _ . For  example : $name, _name, or name <br><br>") ;
+document.write("Variable names are case sensitive <br><br>") ;
+document.write("Variable names should not be JS keywords") ;
+
+
+//chp # 5//
+// Question 1//
+//Addition//
+var num1 = 3 ;
+var num2 = 5 ;
+var result = num1 + num2 ;
+document.write ("the sum of " + num1 + " and " + num2 +" is " + result) ;
+
+// Question 2//
+//subtraction//
+var num1 = 6 ;
+var num2 = 2 ;
+var result = num1 - num2 ;
+document.write ("the subtraction of " + num1 + " in  " + num2 +" is " + result) ;
+
+//division//
+var num1 = 6 ;
+var num2 = 2 ;
+var result = num1 / num2 ;
+document.write ("the division of " + num1 + " and " + num2 +" is " + result) ;
+
+//multiplication//
+var num1 = 6 ;
+var num2 = 2
+var result = num1 * num2 ;
+document.write ("the multiplication of " + num1 + " and " + num2 +" is " + result) ;
+
+//modulus//
+var num1 = 3 ;
+var num2 = 5 ;
+var result = num1 % num2 ;
+document.write ("the modulus of " + num1 + " and " + num2 +" is " + result) ;
+
+
+// Question 3//
+var value_after_declaration = "undefined" ;
+document.write("Value after variable declaration is " + value_after_declaration + "<br><br>") ;
+
+var initial_value = 5 ;
+document.write("Initial Value: " + initial_value + "<br><br>") ;
+
+var increment_value = 6 ;
+document.write("Value after increment is: " + increment_value + "<br><br>")
+
+var add_value = 7
+var add_value_result = add_value + increment_value ;
+document.write("Value after addition is: " + add_value_result + "<br><br>") ;
+
+var decrement_value = 12 ;
+document.write("Value after decrement is: " + decrement_value + "<br><br>") ;
+
+var divide_value = 3 ;
+var result = decrement_value % divide_value ;
+document.write("The Remainder is: " + result) ;
+
+
+// Question 4//
+var one_ticket = 600 ;
+var buying_ticket = 5 ;
+var result = one_ticket * buying_ticket ;
+document.write("Total cost of buying 5 tickets to a movie is " + result + " PKR") ;
+
+
+// Question 5//
+document.write("<h1>Table of 4</h1>")
+var taBLe = 4
+var vaLue = 1
+var result = taBLe * vaLue
+document.write(taBLe + " * " + vaLue + " = " + result +"<br><br>")
+
+var taBLe = 4
+var vaLue = 2
+var result = taBLe * vaLue
+document.write(taBLe + " * " + vaLue + " = " + result +"<br><br>")
+
+var taBLe = 4
+var vaLue = 3
+var result = taBLe * vaLue
+document.write(taBLe + " * " + vaLue + " = " + result +"<br><br>")
+
+var taBLe = 4
+var vaLue = 4
+var result = taBLe * vaLue
+document.write(taBLe + " * " + vaLue + " = " + result +"<br><br>")
+
+var taBLe = 4
+var vaLue = 5
+var result = taBLe * vaLue
+document.write(taBLe + " * " + vaLue + " = " + result +"<br><br>")
+
+var taBLe = 4
+var vaLue = 6
+var result = taBLe * vaLue
+document.write(taBLe + " * " + vaLue + " = " + result +"<br><br>")
+
+var taBLe = 4
+var vaLue = 7
+var result = taBLe * vaLue
+document.write(taBLe + " * " + vaLue + " = " + result +"<br><br>")
+
+var taBLe = 4
+var vaLue = 8
+var result = taBLe * vaLue
+document.write(taBLe + " * " + vaLue + " = " + result +"<br><br>")
+
+var taBLe = 4
+var vaLue = 9
+var result = taBLe * vaLue
+document.write(taBLe + " * " + vaLue + " = " + result +"<br><br>")
+
+var taBLe = 4
+var vaLue = 10
+var result = taBLe * vaLue
+document.write(taBLe + " * " + vaLue + " = " + result +"<br><br>")
+
+
+// Question 6//
+var celsius_temperature = "25°C"
+var result = (25 * 9 / 5) +32
+document.write(celsius_temperature + " is " + result + "°F <br><br>")
+
+var fahranheit_temperature = "70°F"
+var result = (70 - 32) * 5 / 9
+document.write(fahranheit_temperature + " is " + result + "°C")
+
+
+// Question 7//
+document.write("<h1>Shopping Cart</h1> <br><br>") ;
+var iTem1 = 650 ;
+document.write("Price of item 1 is " + iTem1 + "<br><br>") ;
+
+var quantity_item1 = 3 ;
+document.write("Quantity of item1 is " + quantity_item1 + "<br><br>") ;
+
+var iTem2 = 100 ;
+document.write("Price of item 2 is " + iTem2 + "<br><br>") ;
+
+var quantity_item2 = 7 ;
+document.write("Quantity of item 2 is " + quantity_item2 + "<br><br>") ;
+
+var shiPPing = 100 ;
+document.write("Shipping Charges is " + shiPPing + "<br><br>") ;
+
+var result = iTem1*3 + iTem2*7 + shiPPing ;
+document.write("Total cost of your order is " + result ) ;
+
+
+// Question 8//
+var total_marks = 980 ;
+document.write("Total Marks: " + total_marks + "<br><br>")
+
+var obtained_marks = 804 ;
+document.write("Obtained Marks: " + obtained_marks + "<br><br>")
+
+var result = (obtained_marks / total_marks) * 100 ;
+document.write("percentage " + result + "%") ;
+
+
+// Question 9//
+document.write("<h1>Currency in PKR</h1> <br><br>")
+var us_dollar = 104.80 ;
+var saudi_riyal = 28 ;
+var result = 104.80 * 10 + 28 * 25 ;
+document.write("Total Currency in PKR: " + result)
+
+
+// Question 10//
+var initial_value = 2 ;
+var add_value = 5 ;
+var multiply_value = 10 ;
+var result = (initial_value + add_value) * 10 /2 ;
+document.write (result)
+
+
+// Question 11//
+document.write("<h1>Age Calculator</h1> <br><br>") ;
+
+var current_year = 2022 ;
+document.write("Current Year :"+ current_year +"<br><br>") ;
+
+var birth_year = 2002 ;
+document.write("Birth Year :"+ birth_year +"<br><br>") ;
+
+var result = current_year - birth_year ;
+document.write("Your Age is: " + result) ;
+
+
+// Question 12//
+var radius = 20 ;
+document.write("Radius of a circle: " + radius + "<br><br>") ;
+var calculate_circumference = 2 * 3.142 * 20 ;
+document .write ("The circumference is: " + calculate_circumference + "<br><br>") ;
+var calculate_area = 3.142 * 400;
+document .write ("The area is: " + calculate_area) ;
+
+
+// Question 13//
+var favourite_snkae = "Choclate Chips" ;
+document.write("Favourite Snack is: " + favourite_snkae + "<br><br>") ;
+var current_age = 15 ;
+document.write("Current Age: " + current_age + "<br><br>") ;
+var maximun_age = 65 ;
+document.write("Estimated Maximum Age: " + maximun_age + "<br><br>") ;
+var amount_per_day = 3 ;
+document.write("Amount of snacks per day: " + amount_per_day + "<br><br>") ;
+
+var result =amount_per_day *(maximun_age - current_age) ;
+document.write("You will need " + result + " choclate chips to last you until the ripe old age of 65") ;
+
+
+
+//chp # 6//
+//question 1//
